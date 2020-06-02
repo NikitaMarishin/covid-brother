ea470c3c2ccddb53a22cc22a604dfe5f1d28f41e
hiroo <n.s.marishin@gmail.com>
2020-06-02 16:46:32 +0700

    Инициализация репозитория

---
+++ src/main/java/ru/kremlin/covidbrother/CovidBrotherApplication.java
@@ -0,0 +1,13 @@
+package ru.kremlin.covidbrother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidBrotherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidBrotherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/kremlin/covidbrother/CovidBrotherApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kremlin.covidbrother;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidBrotherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
